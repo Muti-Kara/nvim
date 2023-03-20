f132fa596e4d4009a60ac94fa44d42fc91b1e5ce
Muti-Kara <mutikara2007@gmail.com>
2023-03-20 23:50:04 +0300

    generalized

--- lua/config/treesitter.lua
+++ lua/config/treesitter.lua
@@ -1,7 +1,7 @@
 local configs = require("nvim-treesitter.configs")
 
 configs.setup {
-	ensure_installed = {'lua', 'vim', 'regex', 'bash', 'markdown', 'markdown_inline', 'python'},
+	ensure_installed = {'lua', 'vim', 'regex', 'markdown', 'markdown_inline'},
 	sync_install = false, 
 	ignore_install = { "" },
 	highlight = {
--- lua/lsp/mason.lua
+++ lua/lsp/mason.lua
@@ -1,5 +1,5 @@
 local servers = {
-	"pylsp",
+	-- "pylsp",
 }
 
 local settings = {
